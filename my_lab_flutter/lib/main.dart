718dba95faa55f115d5c6e59b9f526c55c080551
Suriya <suriya.sonphu@gmail.com>
2021-04-17 22:30:16 +0700

    Add widget as column and align center.

--- my_lab_flutter/lib/main.dart
+++ my_lab_flutter/lib/main.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "My Data",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My Data"),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Data",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "My Factory",
+            style: TextStyle(color: Colors.grey.shade800),
+          ),
+        ),
+        body: Center(
+          // child: Image(
+          //   image: NetworkImage(
+          //       "https://image.freepik.com/free-vector/group-analysts-working-graphs_1262-21249.jpg"),
+          // ),
+          // child: Text(
+          //   "Data is being updated. Please wait.",
+          //   style: TextStyle(color: Colors.grey, fontSize: 30),
+          // ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Data is being updated.",
+                style: TextStyle(color: Colors.grey.shade600, fontSize: 30),
+              ),
+              Text(
+                "Please be patient.",
+                style: TextStyle(color: Colors.grey.shade600, fontSize: 30),
+              ),
+              Image(
+                image: NetworkImage(
+                    "https://image.freepik.com/free-vector/group-analysts-working-graphs_1262-21249.jpg"),
+                width: 200,
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
-  runApp(app);
+      theme: ThemeData(primarySwatch: Colors.amber),
+    );
+  }
 }
