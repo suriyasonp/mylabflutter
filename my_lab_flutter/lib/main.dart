4d9de665466133deaa69b65ac3a7344ed2d7fb3b
Suriya <suriya.sonphu@gmail.com>
2021-04-18 15:37:29 +0700

    add floating button and add event increase number value.

--- my_lab_flutter/lib/main.dart
+++ my_lab_flutter/lib/main.dart
@@ -9,42 +9,65 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My Factory",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "My Factory",
-            style: TextStyle(color: Colors.grey.shade800),
-          ),
-        ),
-        body: Center(
-          // child: Image(
-          //   image: NetworkImage(
-          //       "https://image.freepik.com/free-vector/group-analysts-working-graphs_1262-21249.jpg"),
-          // ),
-          // child: Text(
-          //   "Data is being updated. Please wait.",
-          //   style: TextStyle(color: Colors.grey, fontSize: 30),
-          // ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Data is being updated.",
-                style: TextStyle(color: Colors.grey.shade600, fontSize: 30),
-              ),
-              Text(
-                "Please be patient.",
-                style: TextStyle(color: Colors.grey.shade600, fontSize: 30),
-              ),
-              Image(
-                image: AssetImage('assets/images/data.jpg'),
-                width: 200,
-              )
-            ],
-          ),
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.amber),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My Factory",
+          style: TextStyle(color: Colors.grey.shade800),
         ),
       ),
-      theme: ThemeData(primarySwatch: Colors.amber),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Data is being updated.",
+              style: TextStyle(color: Colors.grey.shade600, fontSize: 30),
+            ),
+            Text(
+              "Please wait.",
+              style: TextStyle(color: Colors.grey.shade600, fontSize: 30),
+            ),
+            Image(
+              image: AssetImage('assets/images/data.jpg'),
+              width: 200,
+            ),
+            Text(''),
+            Text("Press a button to increase a number.",
+                style: TextStyle(color: Colors.grey.shade600, fontSize: 18)),
+            Text(number.toString(),
+                style: TextStyle(color: Colors.grey.shade600, fontSize: 35))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (number >= 10) {
+              number = 0;
+            } else {
+              number++;
+            }
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
